19dedb6908c9d28aaf062f834c2efb1e502fe0db
matrix <hongsong.tan@mcirofun.com>
2016-07-30 15:15:01 +0800

    －update

--- Resources/projects/shader_twist_quick_frag.glsl
+++ Resources/projects/shader_twist_quick_frag.glsl
@@ -6,6 +6,9 @@ varying vec2 v_texcoord;
 void main(void)
 {
     float time = u_time + u_random_start;
-    vec2 uv = v_texcoord + vec2(cos(time * 33.0), sin(time * 22.0)) * 0.02;
-    gl_FragColor = texture2D(CC_Texture0 , uv);
+    vec2 uv = v_texcoord + vec2(cos(time * 43.0), sin(time * 28.0)) * 0.05;
+    vec4 color = texture2D(CC_Texture0 , uv);
+    float timeFactor = sin(u_time * 5.0) * 3.8 + 1.0;
+    timeFactor = max(timeFactor, 1.0);
+    gl_FragColor = vec4(color.xyz * timeFactor, color.a);
 }
